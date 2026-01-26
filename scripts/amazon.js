e1e0434aaf6e8dc78876c3f39585b58d9fedeb3c
Varshitha HM <varshitha.hm@synchronoss.com>
2026-01-26 12:04:18 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,7 +82,16 @@ document.querySelectorAll('.js-add-to-cart')
           quantity:1
         });
       }
-       console.log(cart);   
+    
+    
+      // let cart section interactive
+      let cartQuantity = 0 ;
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+       });
+
+       document.querySelector('.js-cart-quantity')
+       .innerHTML = cartQuantity;
     })
   }
 )
