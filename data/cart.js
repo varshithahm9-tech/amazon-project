3ab4c4e87d95a73dee6f69e30dbff00c56eaaf58
Varshitha HM <varshitha.hm@synchronoss.com>
2026-01-26 14:30:57 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+ [
+  
+  
+  {
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity:2,
     },
@@ -7,6 +11,11 @@ export let cart = [{
         quantity:1, 
     }
 ];
+
+
+function saveToStorage() {
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
       let matchingItem;
@@ -26,6 +35,8 @@ export function addToCart(productId) {
           quantity:1
         });
       }
+
+      saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -38,4 +49,5 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+    saveToStorage();
 }
