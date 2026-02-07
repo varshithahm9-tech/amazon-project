d67d208ae60b202b83f89566bb3bde49ee520628
Varshitha HM <varshitha.hm@synchronoss.com>
2026-02-07 16:27:56 +0530

    learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -118,3 +118,17 @@ export function getCartQuantity() {
 
     return cartQuantity;
 }
+
+
+
+export function loadCart(fun) {
+  const xhr =  new XMLHttpRequest();
+
+  xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET','https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,10 +3,59 @@ import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
+import { loadCart } from "../data/cart.js";
+
+// promises help keep our code flat
+// use promises instead of callbacks
+
+// promise.all() ==> run mul promises at the same time
+//  and wait for all of them to finish
+
+Promise.all([
+    new Promise((resolve)=>{
+    loadProducts(() =>{
+        // wait each steps to finish
+        resolve('value1');
+    });
+    }),
+
+    new Promise((resolve) =>{
+        loadCart(()=>{
+            resolve();
+        });   
+    })
+
+]).then((values)=>{
+    console.log(values);
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+
+// new Promise((resolve)=>{
+//     loadProducts(() =>{
+//         // wait each steps to finish
+//         resolve('value1');
+//     });
+// }).then((value)=>{
+//     console.log(value); 
+//     return new Promise((resolve) =>{
+//         loadCart(()=>{
+//             resolve();
+//         });   
+//     });
+// }).then(()=>{
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
 
 
 // anonymous fun  ==> fun without name
-loadProducts(()=>{
-    renderOrderSummary();
-    renderPaymentSummary();
-})
+// loadProducts(()=>{
+//     loadCart(()=>{
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
+
+
